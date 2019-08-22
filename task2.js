c2cbf044ee67c2189b6fff5903036c1388283590
slavik3333 <slavik3333@yahoo.com>
2019-08-22 08:49:29 +0300

    Correct task 2

--- task2.js
+++ task2.js
@@ -1,34 +1,48 @@
 //Task 2
 console.log('\n');
 console.log('Task 2')
-const randomNum = function(from, to) {
+const randomNum = function (from, to) {
     return (Math.random() * (to - from) + from).toFixed();
 }
 
-function createArr(numOfNum){
+function createArr(numOfNum) {
     let numArr = [];
-    for(let i = 0; i < numOfNum; i++) {
+    for (let i = 0; i < numOfNum; i++) {
         numArr[i] = parseInt(randomNum(-100, 100))
     }
     return numArr;
 }
 const numOfNum = 100, // Кількість елементів для згенерованого масиву 
-      arrayNew = createArr(numOfNum);
+    arrayNew = createArr(numOfNum);
 
 console.log('Our created random array - ', arrayNew);
 
 const positiveNum = arrayNew
     .filter(item => item >= 0),
-      negativeNum = arrayNew
+    negativeNum = arrayNew
     .filter(item => item < 0);
 
 
 const minNum = Math.min(...arrayNew.map(item => item)),
-      maxNum = Math.max(...arrayNew.filter(item => item)); // більше варіацій, як привязати методи перебирання прив'язати до максимальний і мінімальних значень я не знайшов
+    maxNum = Math.max(...arrayNew.filter(item => item));
 
+let min = arrayNew[0];
+arrayNew.forEach(element => {
+    if (element < min) {
+        min = element;
+    }
+})
 
+let max = arrayNew[arrayNew.length - 1];
+arrayNew.forEach(element => {
+    if (element > max) {
+        max = element;
+    }
+})
 
-console.log('All positive elements from our created array - ', positiveNum);
-console.log('All negative elements from our created array - ', negativeNum);
+console.log('All positive elements from our created array - ', positiveNum.length);
+console.log('All negative elements from our created array - ', negativeNum.length);
 console.log('Min element - ', minNum);
 console.log('Max element - ', maxNum);
+console.log('Min element - ', min);
+console.log('Max element - ', max);
